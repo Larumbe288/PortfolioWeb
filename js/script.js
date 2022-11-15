c8048560926ae5f739ed066f3a0527c64fc5015f
Álvaro Larumbe <alvarolarumbe97@gmail.com>
2022-11-15 18:38:52 +0100

    aniado pagina con footer mas o menos maquetada

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     let entradilla = document.getElementById("entradilla");
-    let aleatorio = Math.floor(Math.random()*3)+1;
+    let aleatorio = Math.floor(Math.random() * 5) + 1;
     switch (aleatorio) {
         case 1:
             entradilla.innerText = "A forward thinking makes a better world";
@@ -9,8 +9,14 @@ window.onload = function () {
         case 2:
             entradilla.innerText = "Keep your eyes on the stars, and your feet on the ground";
             break;
+        case 3:
+            entradilla.innerText = "The power of imagination makes us infinite";
+            break;
+        case 4:
+            entradilla.innerText = "The only goal you can’t accomplish is the one you don’t go after";
+            break;
         default:
             entradilla.innerText = "You only fall when you stop trying";
-            break; 
+            break;
     }
 }
